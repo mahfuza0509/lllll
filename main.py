0df77077b88e899feedab9270729d1135f820434
mahfuza0509 <baltabaevanina0@gmail.com>
2024-07-26 17:43:01 +0500

    python tugatish

--- main.py
+++ main.py
@@ -47,3 +47,10 @@ print("  ****************")
 print("   **************")
 print("     **********")
 print("        ***")
+    <!-- echo "# lllll" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/mahfuza0509/lllll.git
+git push -u origin main -->
