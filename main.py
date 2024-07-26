08f2c5c74bbcb0cad6063e3f20bec64dcc472ae9
mahfuza0509 <baltabaevanina0@gmail.com>
2024-07-26 17:19:21 +0500

    big project

---
+++ main.py
@@ -0,0 +1,49 @@
+print("******")
+print("******")
+print("******")
+
+print("\n")
+
+print("   *")
+print("  ***")
+print(" *****")
+print("*******")
+
+print("\n")
+
+print("   |   ")
+print("-------")
+print("   |   ")
+
+print("\n")
+
+print("-------")
+
+print("\n")
+
+print("            *")
+print("          * *")
+print("        *   *")
+print("      *     *")
+print("    *       *")
+print("  *         *")
+print("*           *")
+print("*************")
+
+print("\n")
+
+print("        ***")
+print("     **     **")
+print("   **         **")
+print("  *             *")
+print("   **         **")
+print("     **     **")
+print("        ***")
+print("\n")
+print("        ***")
+print("     *********")
+print("   *************")
+print("  ****************")
+print("   **************")
+print("     **********")
+print("        ***")
